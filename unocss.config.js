9f9ac2828e28ede83d58655b2d22be88899843f5
chenhu <chenhu@reedsec.com>
2022-09-27 11:58:26 +0800

    update

--- unocss.config.js
+++ unocss.config.js
@@ -2,6 +2,10 @@ import {
   defineConfig
 } from "unocss";
 import presetWeapp from 'unocss-preset-weapp'
+import {
+  transformerAttributify,
+  transformerClass
+} from 'unocss-preset-weapp/transformer'
 export default defineConfig({
   include,
   presets: [
