c41c49bfe0cc618bc664d511ed145eef1b62f7e0
chenhu <chenhu@reedsec.com>
2022-09-27 14:55:43 +0800

    fix : 修复unocss无法启动

--- unocss.config.js
+++ unocss.config.js
@@ -2,9 +2,22 @@ import {
   defineConfig
 } from "unocss";
 import presetWeapp from 'unocss-preset-weapp'
+import {
+  defaultAttributes,
+  defaultIgnoreNonValuedAttributes,
+  transformerAttributify,
+  transformerClass
+} from 'unocss-preset-weapp/transformer'
 export default defineConfig({
-  include,
   presets: [
     presetWeapp(),
+  ],
+  transformers: [
+    // options 见https://github.com/MellowCo/unocss-preset-weapp/tree/main/src/transformer/transformerAttributify
+    transformerAttributify({
+      attributes: [...defaultAttributes]
+    }),
+    // options 见https://github.com/MellowCo/unocss-preset-weapp/tree/main/src/transformer/transformerClass
+    transformerClass(),
   ],
 })
